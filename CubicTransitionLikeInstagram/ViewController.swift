9d160cf6b2e2c772ddc2aa3f37d94e40283919cf
Manas Mishra <manas1.mishra@ril.com>
2019-03-08 10:40:53 +0530

    Initial

--- CubicTransitionLikeInstagram/ViewController.swift
+++ CubicTransitionLikeInstagram/ViewController.swift
@@ -9,12 +9,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var shouldreverse: Bool = false
 
+    @IBOutlet weak var view1: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func animateCubic() {
+        var t = CATransform3DIdentity
+        t.m34 = 1.0 / -500
+        t = CATransform3DRotate(t, CGFloat(0.3 * .pi), 0, 1, 0)
+        view1.layer.transform = t
+    }
 
-
+    @IBAction func animate(_ sender: Any) {
+        if shouldreverse {
+            
+        } else {
+            animateCubic()
+        }
+    }
+    
 }
 
